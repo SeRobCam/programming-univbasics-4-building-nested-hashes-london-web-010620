37171630fcee5c22abff4fe0e4c2551c7685b7d3
Sergio Robles <sroblesc@gmail.com>
2019-12-12 19:52:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 def monopoly_with_second_tier
   monopoly = Hash.new
   {:railroads => {}}
-  monopoly[:railroads] = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
+  monopoly[:railroads][:pieces = 4]
 end
 
 def monopoly_with_third_tier
