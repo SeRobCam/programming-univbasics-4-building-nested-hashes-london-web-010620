1bfa9f22f641370f0ee7ebd71c5dcba20be90ed0
Sergio Robles <sroblesc@gmail.com>
2019-12-12 20:31:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,13 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-monopoly =  {
-  
-}
+ monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>{}
+  }
+  }
 end
 
 def monopoly_with_fourth_tier
